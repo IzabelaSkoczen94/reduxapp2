0ae00aceb6ae52179a38a2be8d5be9c2c8403b63
IzabelaSkoczen94 <izabela.s94@gmail.com>
2019-06-05 12:46:26 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { PropTypes } from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//function reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {...state, counter: state.counter + 1};
+    case 'DECREMENT':
+      return { ...state, counter: state.counter - 1 };
+    default:
+      return state;
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducer, { counter: 0});
+
+class ManagedCounter extends React.Component {
+  static propTypes = {
+    counter: PropTypes.Number,
+    onIncrement: PropTypes.func,
+    onDecrement: PropTypes.func
+  };
+
+render()  {
+  const { counter, onIncrement, onDecrement } = this.props;
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={onDecrement}>-</button>
+      <button onClick={onIncrement}>+</button>
+    </div>
+  );
+}
+}
+
+const mapStateToProps = (state) => {
+  return state; 
+}
+
+// function increment(value) {
+//   return {
+//     type: 'INCREMENT',
+//     payload: value
+//   }}
+  
+// function decrement(value) {
+//   return {
+//     type: 'INCREMENT',
+//     payload: value
+//   }}
+
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrement: () => dispatch({ type: 'INCREMENT' }),
+    onDecrement: () => dispatch({ type: 'DECREMENT' })
+    // onIncrement: () => increment(),
+    // onDecrement: () => decrement()
+  }
+};
+
+ManagedCounter = connect(mapStateToProps, mapDispatchToProps)(ManagedCounter);
+
+render(
+  <Provider store={store}>
+    <ManagedCounter />
+  </Provider>
+  , document.getElementById('root')
+);
+
+
+
